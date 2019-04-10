3c1d830f6ecd1e5d1ef19131403877b120880565
Patrick Presto <pgpresto@gmail.com>
2019-04-10 16:49:28 -0700

    fixed typos

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flast
+from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -9,9 +9,8 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        app.config.from_pyfile(config.py','silent=True')
+        app.config.from_pyfile('config.py','silent=True')
     else:
         app.config.from_mapping(test_config)
 
     return app
-    
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 db_host = os.environ.get('DB_HOST', default='localhost')
-db_name = os.environ.get('DB_NAME','default='notes')
+db_name = os.environ.get('DB_NAME', default='notes')
 db_user = os.environ.get('DB_USERNAME', default='notes')
 db_password = os.environ.get('DB_PASSWORD', default='')
 db_port = os.environ.get('DB_PORT', default='5432')
