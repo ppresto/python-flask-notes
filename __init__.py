616d2c9baf8cb4a3b9d88ac6bdbb267be7abcead
Patrick Presto <pgpresto@gmail.com>
2019-04-11 17:24:40 +0000

    Added flask sqlalchemy db connector

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
 
+# flas_migrate creates migrations so we can create tables we need and make modifications as we change classes.
+# For example, If we add column to user class
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_mapping(
@@ -12,5 +15,10 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py','silent=True')
     else:
         app.config.from_mapping(test_config)
+
+    from .modules import db
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
     return app
